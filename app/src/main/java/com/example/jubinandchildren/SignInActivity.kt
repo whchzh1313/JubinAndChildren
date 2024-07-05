d382448b853626809607b08f81c0154246d9ef41
mwammwa <blackbearsmuffler@naver.com>
2024-07-05 19:29:47 +0900

    L+20240705 ver 0.1.3. chip group error

--- app/src/main/java/com/example/jubinandchildren/SignInActivity.kt
+++ app/src/main/java/com/example/jubinandchildren/SignInActivity.kt
@@ -1,10 +1,11 @@
 package com.example.jubinandchildren
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,9 @@ import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var checkedChipIds: MutableList<Int?>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,14 +29,18 @@ class SignInActivity : AppCompatActivity() {
             insets
         }
         var signin_name = findViewById<EditText>(R.id.text_signin_putname)
+
         var signin_gender = findViewById<MaterialButtonToggleGroup>(R.id.toggle_button_group)
         var toggle_gender1 = findViewById<MaterialButton>(R.id.toggle_btn_gender1)
         var toggle_gender2 = findViewById<MaterialButton>(R.id.toggle_btn_gender2)
+
         var signin_birth = findViewById<EditText>(R.id.text_signin_putbirth)
+
         var signin_id = findViewById<EditText>(R.id.text_signin_putid)
         var signin_id_check = findViewById<Button>(R.id.btn_signin_check_id)
         var signin_pw = findViewById<EditText>(R.id.text_signin_putpw)
         var signin_pw_check = findViewById<EditText>(R.id.text_signin_putpw_check)
+
         var signin_genre = findViewById<ChipGroup>(R.id.chip_si)
         var signin_genre_chip1 = findViewById<Chip>(R.id.chip_signin_genre1)
         var signin_genre_chip2 = findViewById<Chip>(R.id.chip_signin_genre2)
@@ -40,20 +48,84 @@ class SignInActivity : AppCompatActivity() {
         var signin_genre_chip4 = findViewById<Chip>(R.id.chip_signin_genre4)
         var signin_genre_chip5 = findViewById<Chip>(R.id.chip_signin_genre5)
         var signin_genre_chip6 = findViewById<Chip>(R.id.chip_signin_genre6)
+        var signin_genre_chip7 = findViewById<Chip>(R.id.chip_signin_genre7)
+//        val checkedChipIds = signin_genre.checkedChipIds
+
         val btn_signup = findViewById<Button>(R.id.btn_signin_complete)
-        var str_toggle = String()
+
+        var b_toggle: Boolean? = null
         //성별 토글버튼
+        toggle_gender1.setOnClickListener {
+            b_toggle = true
+        }
+        toggle_gender2.setOnClickListener {
+            b_toggle = false
+        }
+
+        var isChecked: Boolean? = null
+
+        signin_id_check.setOnClickListener {
+            if (signin_id.text.toString() in userDataList.userList) {
+                Toast.makeText(this, "이미 존재하는 아이디입니다.", Toast.LENGTH_SHORT).show()
+                isChecked = false
+            } else {
+                Toast.makeText(this, "중복 확인이 완료 되었습니다.", Toast.LENGTH_SHORT).show()
+                isChecked = true
+            }
+        }
+
+        checkedChipIds = mutableListOf()
+        signin_genre.setOnCheckedStateChangeListener { group, checkedIds ->
+            if (checkedIds.size > 3) {
+                val chip = findViewById<Chip>(checkedIds[checkedIds.size - 1])
+                chip.isChecked = false
+                Toast.makeText(this, "최대 3개의 장르를 선택할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                Log.d("checked", "test${checkedChipIds.size}")
+                Log.d("checked", "test$checkedChipIds")
+            } else {
+                checkedChipIds = checkedIds
+                Log.d("checked", "testElse${checkedChipIds.size}")
+                Log.d("checked", "testElse$checkedChipIds")
+            }
+        }
+
         //회원가입 버튼 눌렀을 때
+
+
         // 조건 : 모든 정보가 기입 되어야 함, 비밀번호와 비밀번호 체크가 동일, 아이디 중복 확인이 통과된 상태에서 끝나야함
         btn_signup.setOnClickListener {
-            if (signin_name.text.toString().trim().isEmpty() || signin_birth.text.toString().trim()
-                    .isEmpty() || signin_id.text.toString().trim()
-                    .isEmpty() || signin_pw.text.toString().trim()
-                    .isEmpty()
-            // 칩 + 토글버튼 입력 추가
-            )
-                Toast.makeText(this, "입력 되지 않은 정보가 존재 합니다.", Toast.LENGTH_SHORT).show()
+            if (signin_name.text.isEmpty()) {
+                Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (signin_id.text.isEmpty()) {
+                Toast.makeText(this, "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (signin_pw.text.isEmpty()) {
+                Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (b_toggle == null) {
+                Toast.makeText(this, "성별을 선택 해주세요.", Toast.LENGTH_SHORT).show()
+            } //다시 확인하기
+            else if (signin_pw.text.trim() != signin_pw_check.text.trim()) {
+                Toast.makeText(this, "비밀번호와 비밀번호 확인이 서로 다릅니다.", Toast.LENGTH_SHORT).show()
+            } else if (isChecked == false || isChecked == null) {
+                Toast.makeText(this, "아이디 중복 확인을 다시 해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (checkedChipIds.size != 3) {
+                Toast.makeText(this, "3가지의 장르를 선택해주세요.", Toast.LENGTH_SHORT).show()
+                Log.d("checked", "$checkedChipIds")
+            } else {
+                val userData = UserData(
+                    signin_id.text.toString(),
+                    signin_name.text.toString(),
+                    b_toggle,
+                    signin_birth.text.toString(),
+                    signin_pw.text.toString(),
+                    checkedChipIds
+                )
+                userDataList.userList[signin_id.text.toString()] = userData
+                val intent = Intent(this, LogInActivity::class.java)
+                intent.putExtra("userData", userData)
+            }
         }
         //칩 최대 3개까지 선택 가능하게
+
+
     }
 }
--- app/src/main/java/com/example/jubinandchildren/UserData.kt
+++ app/src/main/java/com/example/jubinandchildren/UserData.kt
@@ -1,15 +1,18 @@
 package com.example.jubinandchildren
 
-data class UserData(
-    val userName: String,
-    val isMan: Boolean,
-    val userBirth: String,
-//    val userId: String,
-    //유저 리스트 첫번째에 아이디 작성해주기
-    val userPw: String,
-    val userGenre: String
-)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-object UserDataList {
-    val userList: MutableMap<String, UserData> = mutableMapOf("a4cross072" to UserData("공명선",true,"YYYY_MM_DD","gongms",""),)
+@Parcelize
+data class UserData(
+    val userId: String,
+    val userName: String,
+    val isMan: Boolean?,
+    val userBirth: String,
+    val userPw: String,
+    val userGenre: MutableList<Int?>
+):Parcelable
+
+object userDataList {
+    val userList: MutableMap<String, UserData> = mutableMapOf()
 }
