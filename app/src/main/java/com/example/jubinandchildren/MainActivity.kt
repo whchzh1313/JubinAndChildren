5c383f38e2501cc85c6b82aabdd4957bda796a07
mwammwa <blackbearsmuffler@naver.com>
2024-07-06 20:42:07 +0900

    L_20240706 ver 0.2.0. 로그인 추가!!!

--- app/src/main/java/com/example/jubinandchildren/MainActivity.kt
+++ app/src/main/java/com/example/jubinandchildren/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.jubinandchildren
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -12,14 +11,14 @@ import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 private lateinit var viewPager_main_reivew: ViewPager2
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val main_navigation_bar = findViewById<BottomNavigationView>(R.id.main_navigation_bar)
+        overridePendingTransition(R.drawable.slideright, R.drawable.sliderightout)
         viewPager_main_reivew = findViewById(R.id.viewPager_main_reivew)
         val viewPager_main_indicator = findViewById<DotsIndicator>(R.id.viewPager_main_indicator)
         /* 여백, 너비에 대한 정의 */
@@ -32,27 +31,30 @@ class MainActivity : AppCompatActivity() {
             page.translationX = position * -offsetPx
         }
 
-
         viewPager_main_reivew.offscreenPageLimit = 3 // 몇 개의 페이지를 미리 로드 해둘것인지
         viewPager_main_reivew.adapter = ViewPagerAdapter(getReviewList()) // 어댑터 생성
         viewPager_main_indicator.attachTo(viewPager_main_reivew)
         viewPager_main_reivew.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
 
+        // 네비게이터 인텐트
+        val main_navigation_bar = findViewById<BottomNavigationView>(R.id.main_navigation_bar)
         main_navigation_bar.setOnNavigationItemSelectedListener() {
             when (it.itemId) {
                 R.id.home -> {
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    true
+                    false
                 }
                 R.id.search -> {
                     val intent = Intent(this, EventActivity::class.java)
                     startActivity(intent)
+                    overridePendingTransition(R.drawable.slideright, R.drawable.sliderightout)
+                    finish()
                     true
                 }
                 R.id.profile -> {
                     val intent = Intent(this, MyPageActivity::class.java)
                     startActivity(intent)
+                    overridePendingTransition(R.drawable.slideright, R.drawable.sliderightout)
+                    finish()
                     true
                 }
                 else -> {
@@ -60,6 +62,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 }
--- app/src/main/java/com/example/jubinandchildren/SignInActivity.kt
+++ app/src/main/java/com/example/jubinandchildren/SignInActivity.kt
@@ -1,16 +1,15 @@
 package com.example.jubinandchildren
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.RadioGroup
+import android.widget.NumberPicker
 import android.widget.Toast
-import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
@@ -37,31 +36,45 @@ class SignInActivity : AppCompatActivity() {
         var toggle_gender1 = findViewById<MaterialButton>(R.id.toggle_btn_gender1)
         var toggle_gender2 = findViewById<MaterialButton>(R.id.toggle_btn_gender2)
 
-        var signin_birth = findViewById<EditText>(R.id.text_signin_putbirth)
-
         var signin_id = findViewById<EditText>(R.id.text_signin_putid)
         var signin_id_check = findViewById<Button>(R.id.btn_signin_check_id)
         var signin_pw = findViewById<EditText>(R.id.text_signin_putpw)
         var signin_pw_check = findViewById<EditText>(R.id.text_signin_putpw_check)
 
         var signin_genre = findViewById<ChipGroup>(R.id.chip_si)
-        var signin_genre_chip1 = findViewById<Chip>(R.id.chip_signin_genre1)
-        var signin_genre_chip2 = findViewById<Chip>(R.id.chip_signin_genre2)
-        var signin_genre_chip3 = findViewById<Chip>(R.id.chip_signin_genre3)
-        var signin_genre_chip4 = findViewById<Chip>(R.id.chip_signin_genre4)
-        var signin_genre_chip5 = findViewById<Chip>(R.id.chip_signin_genre5)
-        var signin_genre_chip6 = findViewById<Chip>(R.id.chip_signin_genre6)
-        var signin_genre_chip7 = findViewById<Chip>(R.id.chip_signin_genre7)
+//        var signin_genre_chip1 = findViewById<Chip>(R.id.chip_signin_genre1)
+//        var signin_genre_chip2 = findViewById<Chip>(R.id.chip_signin_genre2)
+//        var signin_genre_chip3 = findViewById<Chip>(R.id.chip_signin_genre3)
+//        var signin_genre_chip4 = findViewById<Chip>(R.id.chip_signin_genre4)
+//        var signin_genre_chip5 = findViewById<Chip>(R.id.chip_signin_genre5)
+//        var signin_genre_chip6 = findViewById<Chip>(R.id.chip_signin_genre6)
+//        var signin_genre_chip7 = findViewById<Chip>(R.id.chip_signin_genre7)
 //        val checkedChipIds = signin_genre.checkedChipIds
 
         val btn_signup = findViewById<Button>(R.id.btn_signin_complete)
+
+        val np_group = findViewById<ConstraintLayout>(R.id.np_group)
+
+        val np_year = findViewById<NumberPicker>(R.id.np_signin_putyear)
+        val np_month = findViewById<NumberPicker>(R.id.np_signin_putmonth)
+        val np_day = findViewById<NumberPicker>(R.id.np_signin_putday)
+
+        np_year.minValue = 1950
+        np_year.maxValue = 2024
+        np_month.minValue = 1
+        np_month.maxValue = 12
+        np_day.minValue = 1
+        np_day.maxValue = 31
+
+        var np_value = np_year.value.toString() +  np_month.value.toString() + np_day.value.toString()
+
 
         var b_toggle: Boolean? = null
         //성별 토글버튼
         toggle_gender1.setOnClickListener {
             b_toggle = true
         }
-        toggle_gender2.setOnClickListener {
+        toggle_gender2.setOnClickListener{
             b_toggle = false
         }
 
@@ -81,28 +94,29 @@ class SignInActivity : AppCompatActivity() {
         //칩 최대 3개까지 선택 가능하게
         checkedChipIds = mutableListOf()
         val firstChip = R.id.chip_signin_genre1
-        signin_genre.setOnCheckedStateChangeListener { group, checkedIds ->
-            val list: List<Int> = (0..6). filter { firstChip+it !in checkedIds }
+        signin_genre.setOnCheckedStateChangeListener{ group, checkedIds ->
+            val list: List<Int> = (0..6).filter { firstChip + it !in checkedIds }
             if (checkedIds.size > 2) {
                 list.forEach {
-                    findViewById<Chip>(firstChip+it).isEnabled = false
+                    findViewById<Chip>(firstChip + it).isEnabled = false
                 }
 //                Toast.makeText(this, "최대 3개의 장르를 선택할 수 있습니다.", Toast.LENGTH_SHORT).show()
 //                Log.d("checked", "test${checkedChipIds.size}")
 //                Log.d("checked", "test$checkedChipIds")
             } else {
                 list.forEach {
-                    findViewById<Chip>(firstChip+it).isEnabled = true
+                    findViewById<Chip>(firstChip + it).isEnabled = true
                 }
                 checkedChipIds = checkedIds
 //                Log.d("checked", "testElse${checkedChipIds.size}")
 //                Log.d("checked", "testElse$checkedChipIds")
             }
         }
-
+        //인트 > 스트링 > if > contains (함수) >  / 정규식 / numpicker
         //회원가입 버튼 눌렀을 때
         // 조건 : 모든 정보가 기입 되어야 함, 비밀번호와 비밀번호 체크가 동일, 아이디 중복 확인이 통과된 상태에서 끝나야함
-        btn_signup.setOnClickListener {
+        // 생년월일 : . 이나 -를 없애서 인트? 값으로 하고 이걸 다시 스트링 8글자 (size)로 확인 한 다음에 통과
+        btn_signup.setOnClickListener{
             if (signin_name.text.isEmpty()) {
                 Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else if (signin_id.text.isEmpty()) {
@@ -111,12 +125,11 @@ class SignInActivity : AppCompatActivity() {
                 Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else if (b_toggle == null) {
                 Toast.makeText(this, "성별을 선택 해주세요.", Toast.LENGTH_SHORT).show()
-            } //다시 확인하기
-            else if (signin_pw.text.trim() != signin_pw_check.text.trim()) {
+            } else if (signin_pw.text.trim() != signin_pw_check.text.trim()) {
                 Toast.makeText(this, "비밀번호와 비밀번호 확인이 서로 다릅니다.", Toast.LENGTH_SHORT).show()
             } else if (isChecked == false || isChecked == null) {
                 Toast.makeText(this, "아이디 중복 확인을 다시 해주세요.", Toast.LENGTH_SHORT).show()
-            } else if (checkedChipIds.size != 3) {
+            } else if (checkedChipIds.size != 2) {
                 Toast.makeText(this, "3가지의 장르를 선택해주세요.", Toast.LENGTH_SHORT).show()
                 Log.d("checked", "$checkedChipIds")
             } else {
@@ -124,19 +137,17 @@ class SignInActivity : AppCompatActivity() {
                     signin_id.text.toString(),
                     signin_name.text.toString(),
                     b_toggle,
-                    signin_birth.text.toString(),
+                    np_value,
                     signin_pw.text.toString(),
                     checkedChipIds
                 )
                 userDataList.userList[signin_id.text.toString()] = userData
-
                 Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
-
                 val intent = Intent(this, LogInActivity::class.java)
                 intent.putExtra("userData", userData)
+                setResult(RESULT_OK, intent)
+                finish()
             }
-            setResult(RESULT_OK, intent)
-            finish()
         }
     }
 }
